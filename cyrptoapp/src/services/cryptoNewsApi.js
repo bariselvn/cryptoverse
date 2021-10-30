4f6c14ddb1be617a3c37467aaf4bc43a8ac0c285
Barış Elvanoğlu <bariselvn@hotmail.com>
2021-10-30 15:33:34 +0300

    Added Crypto News Api and modified news page
    Added Crypto News Api and modified news page  and added search in news page for cryptocurrencies

---
+++ cyrptoapp/src/services/cryptoNewsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '936e6cfc13mshdc7c41ee094e07ap1c9e71jsne7a7ed22bded'
+  };
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com' 
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl}),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
